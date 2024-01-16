d93dfd775139f8701bec83c0793378de39536311
Jun Kimura <rajun1971@gmail.com>
2024-01-16 18:51:08 +0900

    Fixed Buffer Problem.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -55,7 +55,7 @@ export const create_dat_files_task = async () => {
   // To node.js Buffer
   function toBuffer (typed) {
     const ab = typed.buffer;
-    const buffer = new Buffer(ab.byteLength);
+    const buffer = Buffer.allocUnsafe(ab.byteLength);
     const view = new Uint8Array(ab);
     for (let i = 0; i < buffer.length; ++i) {
       buffer[i] = view[i];
